524287933ddb61fa650049b95aa6f7b1f2f36efb
MWYxiaoxiongdi <2054856652@qq.com>
2021-09-27 11:42:15 +0800

    mwy

--- src/router/index.js
+++ src/router/index.js
@@ -1,29 +1,56 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
-
+import { getToken } from '@/utils/user'
+import axios from '@/utils/axios'
 Vue.use(VueRouter)
 
-const routes = [
+var routes = [
+  {
+    path: '/login',
+    component: () => import('@/views/login/index'),
+    hidden: true
+  },
   {
     path: '/',
     name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    component: () => import(/* webpackChunkName: "about" */ '../views/layout/index.vue')
   }
 ]
+axios.post('/api/getRouters').then((res) => {
+  for (var routitem of res.data) {
+    var item={
+      path:routitem.path,
+      name:routitem.name,
+      component: () => import('../views/'+routitem.path+'/index.vue')
+    }
+    routes.push(item);
+  }
+}).catch((err) => {
+  console.log(err)
+})
+
 
 const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
   routes
+})
+
+var whiteList = "/login"
+var hasToken = getToken();
+router.beforeEach((to, from, next) => {
+  //to: 即将要进入的目标 [路由对象]
+  //from:当前导航正要离开的路由
+  //next:一定要调用该方法来 resolve 这个钩子
+  if (hasToken) {
+    next()
+  } else {
+    if (whiteList === to.path) {
+      next()//这里是即将进入的页面是白名单的页面就直接进入
+    } else {
+      next({ path: '/login' })//这里是即将进入的页面不是白名单的页面又没有token的情况下重定向到登录页面进行登录操作
+    }
+  }
 })
 
 export default router
