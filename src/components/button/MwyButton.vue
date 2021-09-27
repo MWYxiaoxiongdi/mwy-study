bf9bfcd4ab88b7494407612b59062b64ce2fed45
MWYxiaoxiongdi <2054856652@qq.com>
2021-09-27 11:43:14 +0800

    mwy

--- src/components/button/MwyButton.vue
+++ src/components/button/MwyButton.vue
@@ -1,7 +1,7 @@
 <template>
-    <button @click="mwyclick" size="mini">
+    <a href="#" @click="mwyclick">
         <slot></slot>
-    </button>
+    </a>
 </template>
 <script>
     import { debounce, fnThrottle } from '../unit/index';
@@ -49,5 +49,75 @@
         }
     }
 </script>
-<style lang="scss" scopes>
+<style lang="scss" scoped>
+    * {
+        margin: 0;
+        padding: 0;
+        font-family: '微软雅黑', sans-serif;
+        box-sizing: border-box;
+    }
+    a {
+        position: relative;
+        padding: 5px;
+        color: #21ebff;
+        text-decoration: none;
+        font-size: 14px;
+        text-transform: uppercase;
+        transition: 0.5s;
+        overflow: hidden;
+        -webkit-box-reflect: below 1px linear-gradient(transparent, #0003);
+    }
+
+    a:hover {
+        background: #21ebff;
+        color: #fff;
+        box-shadow: 0 0 50px #21ebff;
+        transition-delay: 0.5s;
+    }
+
+    a:nth-child(1) {
+        filter: hue-rotate(115deg);
+    }
+
+    a:nth-child(3) {
+        filter: hue-rotate(270deg);
+    }
+
+    a::before {
+        content: '';
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 10px;
+        height: 10px;
+        border-top: 2px solid #21ebff;
+        border-left: 2px solid #21ebff;
+        transition: 0.5s;
+        transition-delay: 0.5s;
+    }
+
+    a:hover::before {
+        width: 100%;
+        height: 100%;
+        transition-delay: 0s;
+    }
+
+    a::after {
+        content: '';
+        position: absolute;
+        right: 0;
+        bottom: 0;
+        width: 9px;
+        height: 9px;
+        border-bottom: 2px solid #21ebff;
+        border-right: 2px solid #21ebff;
+        transition: 0.5s;
+        transition-delay: 0.5s;
+    }
+
+    a:hover::after {
+        width: 100%;
+        height: 100%;
+        transition-delay: 0s;
+    }
 </style>
