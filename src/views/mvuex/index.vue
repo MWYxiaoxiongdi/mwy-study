d76efcf1b87b07d9287f29f5ace231b3fdbbe808
MWYxiaoxiongdi <2054856652@qq.com>
2021-09-27 11:14:32 +0800

    mwy

---
+++ src/views/mvuex/index.vue
@@ -0,0 +1,72 @@
+<template>
+        <div>
+          <div class="home">
+            <span>
+              我的vuex学习
+            </span>
+          </div>
+          <mwy-button @click="deleteitem" :throttle="true" :times="5000">vuex的删除</mwy-button>
+        </div>
+      </template>
+      
+      <script>
+        import MwyButton from '@/components/button/MwyButton.vue'
+        export default {
+          name: 'MVuex',
+          components: {
+            MwyButton
+          },
+          data() {
+            return {
+            }
+          },
+          computed: {
+            catalogues() {
+              return this.$store.getters.catalogues
+            }
+          },
+          mounted() {
+            this.setCatalogue();
+            console.log(this.catalogues)
+          },
+          methods: {
+            deleteitem() {
+              var item = {
+                id: '2', name: "2"
+              };
+              this.$store.commit('CatalogueTree/DeleteCatalogues', item);
+              console.log(this.catalogues)
+            },
+            setCatalogue() {
+              var Catalogue = [
+                {
+                  id: '1', name: "1", children: [
+                    {
+                      id: '1-1',
+                      name: '1-1'
+                    }
+                  ]
+                },
+                {
+                  id: '2', name: "2"
+                },
+                {
+                  id: '3', name: "3", children: [
+                    {
+                      id: '3-1',
+                      name: '3-1',
+                      children: [
+                        {
+                          id: '3-1-1',
+                          name: '3-1-1'
+                        }
+                      ]
+                    }
+                  ]
+                }
+              ];
+              this.$store.commit('CatalogueTree/SetCatalogues', Catalogue);
+            }
+          },
+        }
+      </script>
